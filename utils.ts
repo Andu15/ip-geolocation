1ca774fc069861cbdcbbd691a8c78d88cbde52b5
Andu15 <estefania_8_3@hotmail.com>
2023-03-03 23:59:27 -0500

    create options API

---
+++ utils.ts
@@ -0,0 +1,12 @@
+const options: Object = {
+  method: 'GET',
+  url: 'https://ip-reputation-geoip-and-detect-vpn.p.rapidapi.com/',
+  headers: {
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'ip-reputation-geoip-and-detect-vpn.p.rapidapi.com'
+  }
+};
+
+export {
+  options
+}
