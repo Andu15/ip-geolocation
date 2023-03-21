1c937c1ac02d6b0a5743d322a041d78793538e24
Andu15 <estefania_8_3@hotmail.com>
2023-03-20 23:33:24 -0500

    agregando la lógica del negocio

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const $form = document.querySelector('#form');
+const $inputIp = document.querySelector('#ip');
+const $submit = document.querySelector('#submit');
+const $results = document.querySelector('#results');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd53b43b356msh4db0f5e6c6979f0p122c10jsnf86d585b0941',
+		'X-RapidAPI-Host': 'ip-reputation-geoip-and-detect-vpn.p.rapidapi.com'
+	}
+};
+
+const fetchIp = (ip) => {
+  return fetch(`https://ip-reputation-geoip-and-detect-vpn.p.rapidapi.com/?ip=${ip}`, options)
+  .then(response => response.json())
+	.catch(err => console.error(err));
+}
+
+$form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const { value } = $inputIp
+  if (!value) return
+
+  $submit.setAttribute('disabled', '')
+  $submit.setAttribute('aria-busy', 'true')
+
+  const ipInfo = await fetchIp(value)
+  
+  if(ipInfo) {
+    $results.innerHTML = JSON.stringify(ipInfo, null, 2)
+  }
+
+  $submit.removeAttribute('disabled')
+  $submit.removeAttribute('aria-busy')
+})
